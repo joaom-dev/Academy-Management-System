270e65d13d96fd620431ff4568ddd15083a86ae5
joao Marcos <marcoscmd250@gmail.com>
2026-08-22 19:54:43 -0300

    projeto intregrado com ambiente SPRING BOOT

---
+++ src/test/java/com/juao/sistema_academia_spring/SistemaAcademiaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juao.sistema_academia_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAcademiaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
